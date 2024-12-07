ddf0c324d24b08ff95a3b82f6628fc066e3e3176
Shakir Ahmed Zahedi <shakirahmed84@yahoo.com>
2024-12-07 13:34:05 +0100

    update dropdown

--- src/components/header.js
+++ src/components/header.js
@@ -147,15 +147,41 @@ export default function Header() {
                                 anchorEl={anchorEl}
                                 open={Boolean(anchorEl)}
                                 onClose={handleMenuClose}
-                                PaperProps={{
-                                    elevation: 0,
-                                    sx: {
-                                        mt: 1.5,
-                                        '& .MuiAvatar-root': {
-                                            width: 32,
-                                            height: 32,
+                                slotProps={{
+                                    paper: {
+                                        elevation: 0,
+                                        sx: {
+                                            overflow: 'visible',
+                                            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                                            mt: 1.5,
+                                            '& .MuiAvatar-root': {
+                                                width: 32,
+                                                height: 32,
+                                                ml: -0.5,
+                                                mr: 1,
+                                            },
+                                            '&::before': {
+                                                content: '""',
+                                                display: 'block',
+                                                position: 'absolute',
+                                                top: 0,
+                                                right: 14,
+                                                width: 10,
+                                                height: 10,
+                                                bgcolor: 'background.paper',
+                                                transform: 'translateY(-50%) rotate(45deg)',
+                                                zIndex: 0,
+                                            },
                                         },
                                     },
+                                }}
+                                transformOrigin={{
+                                    horizontal: 'right',
+                                    vertical: 'top',
+                                }}
+                                anchorOrigin={{
+                                    horizontal: 'right',
+                                    vertical: 'bottom',
                                 }}
                             >
                                 <MenuItem onClick={() => navigate('/myPage')}>My Page</MenuItem>
